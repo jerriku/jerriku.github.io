e50dfd3d72c38d5d54c94843266105d6083e50c9
Jeric <fajardo.jeric@yahoo.com>
2021-06-03 23:04:57 +0100

    Change display for projects and add contact feature

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
-const projects = document.getElementsByClassName("projects-link");
-const contentPick = choice => {
-    const contents = [...document.getElementsByClassName("content")];
-    contents.forEach(content => {
-        content.style.display = "none";
-    });
-    switch(choice) {
-        case "calculator":
-            $("#calculator").css("display", "inline-block");
-            break;
-        case "account storage":
-            $("#account-storage").css("display", "inline-block");
-            break;
-        case "keyboard tester":
-            $("#keyboard-tester").css("display", "inline-block");
-            break;
-        case "website #1":
-            $("#website-1").css("display", "inline-block");
-            break;
-        case "typing game":
-            $("#type-game").css("display", "inline-block");
-            break;
-        case "tab storage":
-            $("#tab-storage").css("display", "inline-block");
-            break;
-        case "forex journal":
-            $("#forex-journal").css("display", "inline-block");
-            break;
-        case "discord bot":
-            $("#discord-bot").css("display", "inline-block");
-            break;
-        default:
-            $("#main-page").css("display", "inline-block");
-            break;
-    }
+const underscoreElement = document.getElementById("underscore");
+let count = 0;
+
+function underscoreAnimation() {
+    setTimeout(() => {
+        if (count != 5) {
+            underscoreElement.style.opacity = "1";
+            count++;
+        } else if (count != 0) {
+            underscoreElement.style.opacity = "0";
+            count--;
+        }
+        underscoreAnimation();
+    }, 1000);
 }
 
-$(".projects-link").click(e => {
-    contentPick(e.currentTarget.firstChild.textContent);
-})
+underscoreAnimation();
